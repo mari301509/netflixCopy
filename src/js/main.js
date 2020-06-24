27a3ff6f7313a9c207053beca06aee7c67af34a1
mari <61272828+mari301509@users.noreply.github.com>
2020-06-24 16:27:26 +0900

    Revert "Develop : main visual end"

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,4 @@
 // 진입점 js에 폴리필 삽입. (지우지 마세요!)
-import '@babel/polyfill';
+import "@babel/polyfill";
 
 const css = require('../css/app.scss');
-
-// 황태현
-const mainvis = require('./visual/render.js');
-
-// LIST
-const list = require('./list/render.js');
